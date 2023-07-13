1d33b1480689c783cb14ce0421a426ef4f55d8bd
frojoo <wndyd29605@gmail.com>
2023-07-13 09:27:57 +0900

    config

--- next.config.js
+++ next.config.js
@@ -1,28 +1,14 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "olbm.mypinata.cloud",
-//         port: "",
-//         pathname: "/ipfs/**",
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
-
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.fallback = {
-      encoding: false,
-      bufferutil: false,
-      "utf-8-validate": false,
-    };
-
-    return config;
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "olbm.mypinata.cloud",
+        port: "",
+        pathname: "/ipfs/**",
+      },
+    ],
   },
 };
 
